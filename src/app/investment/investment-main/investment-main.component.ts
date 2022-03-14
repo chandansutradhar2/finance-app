c344ce5fbf24bae6564802b00bfda7f03ca995e6
chandan_naresh <chandansutradhar2@gmail.com>
2022-03-14 16:38:15 +0530

    nested routing with lazy loading demo

---
+++ src/app/investment/investment-main/investment-main.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-investment-main',
+  templateUrl: './investment-main.component.html',
+  styleUrls: ['./investment-main.component.css']
+})
+export class InvestmentMainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/investment/investment.module.ts
+++ src/app/investment/investment.module.ts
@@ -3,6 +3,22 @@ import { MutualFundComponent } from './mutual-fund/mutual-fund.component';
 import { DebtFundComponent } from './debt-fund/debt-fund.component';
 import { AddInvestmentComponent } from './add-investment/add-investment.component';
 import { StocksComponent } from './stocks/stocks.component';
+import { InvestmentMainComponent } from './investment-main/investment-main.component';
+import { SharedModule } from '../shared/shared.module';
+import { RouterModule, Routes } from '@angular/router';
+
+const investUrls: Routes = [
+  {
+    path: '',
+    component: InvestmentMainComponent,
+    children: [
+      { path: 'mutualfunds', component: MutualFundComponent },
+      { path: 'debtfunds', component: DebtFundComponent },
+      { path: 'shares', component: StocksComponent },
+    ],
+  },
+];
+//http://localhost:4200/investment
 
 @NgModule({
   declarations: [
@@ -10,8 +26,9 @@ import { StocksComponent } from './stocks/stocks.component';
     DebtFundComponent,
     AddInvestmentComponent,
     StocksComponent,
+    InvestmentMainComponent,
   ], //every component of this module will be eneterd into dec array
-  imports: [], // all modules/classes this module needs,
-  exports: [], //all component this module want other to use
+  imports: [SharedModule, RouterModule.forChild(investUrls)], // all modules/classes this module needs,
+  exports: [RouterModule], //all component this module want other to use
 })
 export class InvestmentModule {}
