8bd490c2db107e07783df673417f3e1c42a739d5
chandan_naresh <chandansutradhar2@gmail.com>
2022-03-14 17:12:28 +0530

    demo of auth guard implementation

--- src/app/app-routing.routing.ts
+++ src/app/app-routing.routing.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AdminGuard } from './admin/admin.guard';
+import { AuthGuard } from './auth.guard';
 import { AddExpenseComponent } from './expense/add-expense/add-expense.component';
 import { DashboardComponent } from './shared/dashboard/dashboard.component';
 
@@ -8,10 +9,12 @@ const rootUrls: Routes = [
   { path: '', component: DashboardComponent },
   {
     path: 'expenses',
+    canActivate: [AuthGuard],
     component: AddExpenseComponent,
   },
   {
     path: 'investment',
+    canLoad: [AuthGuard],
     loadChildren: () =>
       import('./investment/investment.module').then(
         (investMdl) => investMdl.InvestmentModule
