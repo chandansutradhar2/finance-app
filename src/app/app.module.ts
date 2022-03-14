f297c3ccd0c5492cf7806f40070166d4413e7c09
chandan_naresh <chandansutradhar2@gmail.com>
2022-03-14 15:16:16 +0530

    creating a shared module demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { ExpenseModule } from './expense/expense.module';
-
+import { SharedModule } from './shared/shared.module';
+import { HeaderComponent } from './utlity/header/header.component';
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule, BrowserAnimationsModule, ExpenseModule],
+  declarations: [AppComponent, HeaderComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    SharedModule,
+    ExpenseModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
