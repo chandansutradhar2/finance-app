842becf68c29d8020f3aa731d5c52d2a95f95fc6
chandan_naresh <chandansutradhar2@gmail.com>
2022-03-15 15:09:57 +0530

    ngStyle Demo

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -7,6 +7,8 @@ import { FormControl, Validators } from '@angular/forms';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  h4Styles = {};
+  loginClicked: boolean = false;
   email: FormControl = new FormControl('', [
     Validators.required,
     Validators.email,
@@ -23,9 +25,22 @@ export class LoginComponent implements OnInit {
     });
   }
 
+  applyStyles() {
+    this.h4Styles = {
+      'font-size': this.loginClicked ? 'x-large' : 'xx-large',
+      'font-weight': this.loginClicked ? '500' : '300',
+      'border-color': this.loginClicked ? 'green' : 'red',
+      'border-style': this.loginClicked ? 'dashed' : 'double',
+    };
+  }
   login() {
     this.email.markAsDirty();
     this.password.markAsDirty();
+
+    this.email.valid && this.password.valid
+      ? (this.loginClicked = true)
+      : (this.loginClicked = false);
     console.log(this.email.value, this.password.value);
+    this.applyStyles();
   }
 }
