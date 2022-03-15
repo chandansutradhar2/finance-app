d6394a273d208c587f70e0544132ea80cca3ef9c
chandan_naresh <chandansutradhar2@gmail.com>
2022-03-15 15:56:22 +0530

    using formGroup

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  signUpFormGrp: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.signUpFormGrp = fb.group({
+      firstName: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+      mobileNo: ['', []],
+    });
   }
 
+  ngOnInit(): void {}
+
+  signUp() {}
 }
